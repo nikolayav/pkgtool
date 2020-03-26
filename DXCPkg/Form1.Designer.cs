b2c5fc0b0609c1611e21a8cf51cb6de52eb4af0d
nikolayav <nikolay.avroniev@gmail.com>
2020-03-26 11:56:42 +0200

    Add project files.

---
+++ DXCPkg/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace DXCPkg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fileLocationTextBox = new System.Windows.Forms.TextBox();
+            this.Create = new System.Windows.Forms.Button();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ultraControlContainerEditor1 = new Infragistics.Win.UltraWinEditors.UltraControlContainerEditor(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ultraControlContainerEditor1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileLocationTextBox
+            // 
+            this.fileLocationTextBox.Enabled = false;
+            this.fileLocationTextBox.Location = new System.Drawing.Point(12, 36);
+            this.fileLocationTextBox.Name = "fileLocationTextBox";
+            this.fileLocationTextBox.Size = new System.Drawing.Size(247, 20);
+            this.fileLocationTextBox.TabIndex = 0;
+            // 
+            // Create
+            // 
+            this.Create.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Create.Location = new System.Drawing.Point(276, 87);
+            this.Create.Name = "Create";
+            this.Create.Size = new System.Drawing.Size(75, 23);
+            this.Create.TabIndex = 1;
+            this.Create.Text = "Install";
+            this.Create.UseVisualStyleBackColor = true;
+            this.Create.Click += new System.EventHandler(this.Create_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.button1.Location = new System.Drawing.Point(276, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File:";
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(276, 116);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Uninstall";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 220);
+            this.progressBar1.MarqueeAnimationSpeed = 0;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(339, 23);
+            this.progressBar1.Step = 100;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Location = new System.Drawing.Point(9, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Status";
+            // 
+            // ultraControlContainerEditor1
+            // 
+            this.ultraControlContainerEditor1.ContainingControl = this;
+            this.ultraControlContainerEditor1.Name = "ultraControlContainerEditor1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 87);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(247, 89);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(362, 265);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Create);
+            this.Controls.Add(this.fileLocationTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.ultraControlContainerEditor1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fileLocationTextBox;
+        private System.Windows.Forms.Button Create;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
+        private Infragistics.Win.UltraWinEditors.UltraControlContainerEditor ultraControlContainerEditor1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ DXCPkg/Form1.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Win32.TaskScheduler;
+using System;
+using System.ComponentModel;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DXCPkg {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        private void button1_Click(object sender, EventArgs e) {
+            ofd.Filter = "CMD|*.cmd|BAT|*.bat";
+
+            if (ofd.ShowDialog() == DialogResult.OK) {
+                fileLocationTextBox.Text = ofd.FileName;
+            }
+        }
+
+        private void Create_Click(object sender, EventArgs e) {
+            createSchTask("DXC Install As SYSTEM", "/i");
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            createSchTask("DXC Uninstall As SYSTEM", "/u");
+        }
+
+
+        private void createSchTask(string name, string arg) {
+            if (string.IsNullOrWhiteSpace(fileLocationTextBox.Text)) {
+                MessageBox.Show("Please, select a file.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else {
+                Task tsk = null;
+                string fileLocation = fileLocationTextBox.Text;
+                string proccessName = System.IO.Path.GetFileNameWithoutExtension(fileLocation);
+
+                using (TaskService ts = new TaskService()) {
+                    TaskDefinition td = ts.NewTask();
+                    td.Principal.RunLevel = TaskRunLevel.Highest;
+                    td.RegistrationInfo.Description = "Runs a command-line file with " + arg + " argument.";
+                    td.Settings.DisallowStartIfOnBatteries = false;
+                    ExecAction ex = td.Actions.Add(new ExecAction(fileLocation, arg, null));
+
+                    tsk = ts.RootFolder.RegisterTaskDefinition(name, td, TaskCreation.CreateOrUpdate, "SYSTEM", null, TaskLogonType.ServiceAccount);
+                    tsk.Run();
+                }
+
+                taskRunActions(tsk);
+            }
+        }
+
+        private void lockControls(bool isEnabled) {
+            foreach (Control c in this.Controls) {
+                if (c != progressBar1 && c != label2 && c != fileLocationTextBox) {
+                    c.Enabled = isEnabled;
+                }
+            }
+        }
+        private void taskRunActions(Task tsk) {
+            lockControls(false);
+            if (tsk.Name.Equals("DXC Install As SYSTEM")) {
+                sb.Append("Running Install task...");
+                richTextBox1.Text = sb.ToString();
+            } else if (tsk.Name.Equals("DXC Uninstall As SYSTEM")) {
+                sb.Append("\nRunning Uninstall task...");
+                richTextBox1.Text = sb.ToString();
+            }
+
+            label2.Text = "Running";
+            progressBar1.MarqueeAnimationSpeed = 30;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            progressBar1.Value = 0;
+            backgroundWorker1.RunWorkerAsync(tsk);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e) {
+            Task tsk = e.Argument as Task;
+
+            while (tsk.State != TaskState.Ready) {
+                Thread.Sleep(500);
+            }
+
+            e.Result = tsk;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+
+            progressBar1.Value = 100;
+            progressBar1.Style = ProgressBarStyle.Blocks;
+
+            Task tsk = e.Result as Task;
+
+            if (tsk.Name.Equals("DXC Install As SYSTEM")) {
+                sb.Append("\nInstall task has finished.");
+                richTextBox1.Text = sb.ToString();
+            } else if (tsk.Name.Equals("DXC Uninstall As SYSTEM")) {
+                sb.Append("\nUninstall task has finished.");
+                richTextBox1.Text = sb.ToString();
+            }
+            if (e.Error != null) {
+                MessageBox.Show(e.Error.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            label2.Text = "Ready";
+            lockControls(true);
+
+        }
+    }
+}
